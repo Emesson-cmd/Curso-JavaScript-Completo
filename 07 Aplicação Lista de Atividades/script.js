512f6b05ee4fc5820ac08a485097ae685f0f6cf9
Emesson-cmd <emerssonkavallcante@gmail.com>
2022-04-02 17:18:31 -0300

    Finalização de projeto Pokedéx - usando API

--- 07 Aplicação Lista de Atividades/script.js
+++ 07 Aplicação Lista de Atividades/script.js
@@ -1,12 +1,18 @@
 var listaAtividades = [];
 var checado = "unchecked";
 
+// Ao clicar no botão "Adicionar nova atividades", esse comando será ativado
 function addAtividade(){
+    // Busca o elemento lista-atividades no DOM
+    paiAtividades = document.querySelector('.lista-atividades');
 
+    filhoAtividade = document.querySelectorAll('.lista-atividade-single');
 
+    // Abre caixa de diálogo para o usuário inserir a atividade quer será adicionada na lista
     var atividade = prompt("Qual é a atividade?");
 
-    document.querySelector('.lista-atividades').innerHTML += `
+    // Está adicionando uma nova atividades à lista de atividades de acordo com o que o usuário escreveu na caixa de diálogo.
+    paiAtividades.innerHTML += `
     
         <div class="lista-atividade-single">
             <input type="checkbox"  `+checado+`  name="atividade_concluida" id="atividade_concluida" class="atividade_concluida">
@@ -17,27 +23,23 @@ function addAtividade(){
     
     `;
 
-    for (var i = 0; i < listaAtividades.length; i++){
-
+    // Este código pecorre sobre todo o array (listaAtividades) e atribui ao elemento "checado" o valor "checked" e "unchecked" de acordo com o elemento "marcado" (true ou false)
+    /*for (var i = 0; i < listaAtividades.length; i++){
         if (listaAtividades[i].marcado === "true"){
             checado = "checked";
         } else {
             checado = "unchecked";
         }
-    }
+    }*/
 
     // Este código se refere ao clique na caixa de marcação dos itens da lista. Caso eu marque um item, ele será "checado" e o item da lista será riscado. Se eu desmarcar, o item da lista será desmarcado. 
-
     var atividadesConcluida = document.querySelectorAll('input[name=atividade_concluida]');
-
     for (var i = 0; i < atividadesConcluida.length; i++){
         atividadesConcluida[i].addEventListener('click', function(){
             
             for (var j = 0; j < atividadesConcluida.length; j++){
 
                 if (atividadesConcluida[j].checked){
-                    console.log("Há pelo menos um checkbox marcado! -> " + j);
-
                     document.querySelectorAll('.lista-atividade-single > .nome-atividade')[j].classList.add('tachar-atividade');
 
                     listaAtividades.push(
@@ -46,14 +48,27 @@ function addAtividade(){
                             marcado: true
                         }
                     )
-
                 } else {
-                    console.log("Há pelo menos um checkbox desmarcado! -> " + j);
-
                     document.querySelectorAll('.lista-atividade-single > .nome-atividade')[j].classList.remove('tachar-atividade');
                 }
             }
 
         })
     };
+
+    // Ao adicionar um novo item na lista de atividades, esse elemento receberá um atributo chamado itemId. Esse atributo será utilizado para excluir esse elemento futuramente.
+    //var qtdeFilhos = paiAtividades.childElementCount;
+    // filhoAtividade[length].setAttribute("idnex");
+}
+
+// Função somente para testes
+function teste(){
+    // Verifica o elemento childElementCount
+    /*let listaDeAtividades = document.querySelector(".lista-atividades");
+
+    let qtdeFilhos = listaDeAtividades.childElementCount;
+
+    alert(qtdeFilhos)*/
+
+    // Verifica se o atributo index foi adicionado ao elemento
 }
--- 08 App pedra papel ou tesoura/script.js
+++ 08 App pedra papel ou tesoura/script.js
@@ -1,12 +1,68 @@
 var elementos = document.querySelectorAll('.player-options div > img');
-
+var elementosEnemy = document.querySelectorAll('.enemy-options div > img');
 var playerOpt = "";
+var enemyOpt = "";
 
-//alert(elementos.length)
+function validarVitoria(){
+    var vencedor = document.querySelector('.vencedor');
+    
+    if(playerOpt == "rock"){
+        if (enemyOpt == "rock"){
+            vencedor.innerHTML = "Empate";
+        } else if (enemyOpt == "paper"){
+            vencedor.innerHTML = "Inimigo ganhou";
+        } else if (enemyOpt == "scisor"){
+            vencedor.innerHTML = "Inimigo perdeu";
+        } 
+    }
+
+    if(playerOpt == "paper"){
+        if (enemyOpt == "rock"){
+            vencedor.innerHTML = "Inimigo perdeu";
+        } else if (enemyOpt == "paper"){
+            vencedor.innerHTML = "Empate";
+        } else if (enemyOpt == "scisor"){
+            vencedor.innerHTML = "Inimigo ganhou";
+        } 
+    }
+
+    if(playerOpt == "scisor"){
+        if (enemyOpt == "rock"){
+            vencedor.innerHTML = "Inimigo ganhou";
+        } else if (enemyOpt == "paper"){
+            vencedor.innerHTML = "Inimigo perdeu";
+        } else if (enemyOpt == "scisor"){
+            vencedor.innerHTML = "Empate";
+        } 
+    }
+}
 
 function resetOpacityPlayer(){
     for (var i = 0; i < elementos.length; i++){
         elementos[i].style.opacity = 0.3;
+    }
+}
+
+function resetOpacityEnemy(){
+    for (var i = 0; i < elementosEnemy.length; i++){
+        elementosEnemy[i].style.opacity = 0.3;
+    }
+}
+
+function inimigoJoga(){
+    resetOpacityEnemy();
+    
+    // Math.random() -> Numero decimal entre 0 e 1
+    // Math.floor() -> Remove valor flutuante
+    let rand = Math.floor(Math.random()*3);
+
+    const enemyOptions = document.querySelectorAll('.enemy-options div');
+
+    for (var i = 0; i < enemyOptions.length; i++){
+        if (i == rand){
+            enemyOptions[i].childNodes[0].style.opacity = 1;
+            enemyOpt = enemyOptions[i].childNodes[0].getAttribute('opt');
+        }
     }
 }
 
@@ -15,23 +71,9 @@ for (var i = 0; i < elementos.length; i++){
         resetOpacityPlayer();
         t.target.style.opacity = 1;
         playerOpt = t.target.getAttribute('opt');
-    })
-}
 
-var elementosEnemy = document.querySelectorAll('.enemy-options div > img');
+        inimigoJoga();
 
-var enemyOpt = "";
-
-function resetOpacityEnemy(){
-    for (var i = 0; i < elementosEnemy.length; i++){
-        elementosEnemy[i].style.opacity = 0.3;
-    }
-}
-
-for (var i = 0; i < elementosEnemy.length; i++){
-    elementosEnemy[i].addEventListener('click', function(t){
-        resetOpacityEnemy();
-        t.target.style.opacity = 1;
-        enemyOpt = t.target.getAttribute('opt');
+        validarVitoria();
     })
 }
